ce03dfd1f03fa0d218301df2a7db1a2d7535391d
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-09-25 16:10:24 -0400

    Update teddy_functions.jl

--- teddy_functions.jl
+++ teddy_functions.jl
@@ -4,7 +4,7 @@
 function get_each_element(mtrx, dim)
   for i in range(1, dim)
     for j in range(1, dim)
-     print("a[$i, $j]  = ", a[i, j], "\n")
+     print("mtrx[$i, $j]  = ", mtrx[i, j], "\n")
     end
   end
 end
