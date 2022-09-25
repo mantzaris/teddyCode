9b80351c4d1a71f0d9f8e6cf687b47f3100db7b5
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-09-25 15:28:59 -0400

    Create teddy_functions.jl

---
+++ teddy_functions.jl
@@ -0,0 +1,10 @@
+#functions 
+
+#get_row_columns_element
+function get_each_element(mtrx, dim)
+  for i in range(1, dim)
+    for j in range(1, dim)
+     print("a[$i, $j]  = ", a[i, j], "\n")
+    end
+  end
+end
