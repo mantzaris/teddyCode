403d954ea559f1a40b9b9678a41251852fa5999d
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-10-03 01:28:35 -0400

    Update teddy_functions.jl

--- teddy_functions.jl
+++ teddy_functions.jl
@@ -8,3 +8,28 @@ function get_each_element(mtrx, dim)
     end
   end
 end
+
+
+function make_mtrx(dimentions, list_1, list_2)
+  m = zeros(dimentions, dimentions)
+  for i in list_1
+    for j in list_2
+      m[i, j] = 1
+    end
+  end
+  return m
+end
+
+
+function get_zeros_non_zeros(mtrx, dims)
+  s = 0
+    for i in range(1, dims)
+      for j in range(1, dims)
+        if (a[i, j] == 1.0)
+          s = s + 1
+        end
+      end
+    end
+    print("Non zeros: $s and zeros: ", dimentions * dimentions - s)
+  return s
+end
