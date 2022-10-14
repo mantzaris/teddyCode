254a8e324eba5a0b676ea6dbf988a810c0151b7e
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-10-13 21:31:21 -0400

    Update teddy_functions.jl

--- teddy_functions.jl
+++ teddy_functions.jl
@@ -143,3 +143,62 @@ function multiply_splited(a_dim, a_down_lim, a_up_lim, x_y_dim, x_down_lim, x_up
   end
 return(final_mtrx_splited)
 end
+
+
+function dictionary_to_matrix(dict, x_splited_length, x_y_dim, x_mtrx, n_lines_to_split)
+  i = 1 
+  mtrx_reduced_dictionary = Dict()
+  count = 1
+  num = div(size(x_mtrx, 1), n_lines_to_split)
+  while (i <= length(dict)) 
+    s = zeros(num, x_y_dim)
+    k = 1 
+    while k <= x_splited_length  
+      s = s + dict["aj_x_$i"] 
+      k = k + 1 
+      i = i + 1
+    end 
+  mtrx_reduced_dictionary["A_$count"] = s
+  count = count + 1
+  end 
+  s_ = vcat(mtrx_reduced_dictionary["A_1"])
+  for i in range(2, length(mtrx_reduced_dictionary))
+    aux = vcat(s_, mtrx_reduced_dictionary["A_$i"])
+    s_ = aux
+  end
+  return s_ 
+end 
+
+
+function multiply_splited(a_dim, a_down_lim, a_up_lim, x_y_dim, x_down_lim, x_up_lim, th_y_dim, th_down_lim, th_up_lim, n_lines_to_split)
+  a_mtrx = initial_matrix(a_dim, a_down_lim, a_up_lim, x_y_dim, x_down_lim, x_up_lim, th_y_dim, th_down_lim, th_up_lim)[1]
+  aj_mtrx = initial_matrix(a_dim, a_down_lim, a_up_lim, x_y_dim, x_down_lim, x_up_lim, th_y_dim, th_down_lim, th_up_lim)[2]
+  x_mtrx = initial_matrix(a_dim, a_down_lim, a_up_lim, x_y_dim, x_down_lim, x_up_lim, th_y_dim, th_down_lim, th_up_lim)[3]
+  th_mtrx = initial_matrix(a_dim, a_down_lim, a_up_lim, x_y_dim, x_down_lim, x_up_lim, th_y_dim, th_down_lim, th_up_lim)[4]
+  final_mtrx = initial_matrix(a_dim, a_down_lim, a_up_lim, x_y_dim, x_down_lim, x_up_lim, th_y_dim, th_down_lim, th_up_lim)[5]
+
+  a_splited = split_alpha_matrix(a_mtrx, n_lines_to_split)
+  x_splited = split_x_matrix(x_mtrx, n_lines_to_split)
+  aj_splited = Dict()
+  print("\n")
+  for i in range(1, length(a_splited))
+    aj_splited["aj_$i"] = A2S(a_splited["a_$i"])
+  end
+  #show(stdout, "text/plain", aj_splited)
+  aj_x = Dict()
+  k = 1
+  while k <= length(a_splited)
+    for i in range(1, length(x_splited))
+      aj_x["aj_x_$k"] = aj_splited["aj_$k"] * x_splited["x_$i"] 
+      k = k + 1
+    end
+  end
+  #for i in range(1, length(aj_x))
+  #  show(stdout, "text/plain", aj_x["aj_x_$i"])
+  #  print("\n")
+  #end
+  aj_x_mtrx = dictionary_to_matrix(aj_x, length(x_splited), x_y_dim, x_mtrx, n_lines_to_split)
+  print("\n")
+  show(stdout, "text/plain", aj_x_mtrx)
+  return(aj_x_mtrx)
+end
